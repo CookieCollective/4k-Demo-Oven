5a8dfc004a27b8b621aec01b6f7f7619afcb3249
Kushulain <kushulain@gmail.com>
2018-11-03 18:26:15 +0100

    Adds multiple buffers/render targets and backbuffer sampling features. And multiple buffer shader example.

--- engine/entry.cpp
+++ engine/entry.cpp
@@ -131,10 +131,36 @@ void entry()
 	uniformResolutionWidth = (float)width;
 #endif
 
+#ifdef BUFFERS
+  //As we use 2 textures for each buffer for each buffer, "swapped" will tell which texture to render, changes at each frame
+  //dualbuffering prevents reading and writing to the same render target
+	bool swapped;
+
+  //Here we create textures, you can change the size of textures, filtering, add mipmaps, to suit your need
+	GLuint textureID[BUFFERS*2];
+  glGenTextures(BUFFERS*2,textureID);
+
+	for (int i=0; i<BUFFERS*2; i++)
+	{
+		glBindTexture(GL_TEXTURE_2D, textureID[i]);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_FLOAT, NULL);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	}
+
+  //Only one framebuffer to save line of codes, we change the render targets for each buffer in the rendering loop
+	unsigned int fbo;
+	glGenFramebuffers(1, &fbo);
+#endif
+
 #ifdef DEBUG
     //this displays the indices for each uniform, it helps if you want to hardcode indices in the render loop to save line of code for release version
     PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC)wglGetProcAddress("glGetUniformLocation");
     printf ("uniform id for _ : %ld\n", glGetUniformLocation(program,"_"));
+    printf ("uniform id for PASSINDEX : %ld\n", glGetUniformLocation(program,"PASSINDEX"));
+    printf ("uniform id for b0 : %ld\n", glGetUniformLocation(program,"b0"));
+    printf ("uniform id for b1 : %ld\n", glGetUniformLocation(program,"b1"));
+    printf ("uniform id for b2 : %ld\n", glGetUniformLocation(program,"b2"));
 #endif
 
 	audioStart();
@@ -151,9 +177,39 @@ void entry()
 
 		// Assume that the uniforms u[] will always be linked to locations [0-n].
 		// Given that they are the only uniforms in the shader, it is likely to work on all drivers.
-		glUniform1fv(0, UNIFORM_FLOAT_COUNT, uniforms);
+#ifdef BUFFERS
+      glBindFramebuffer (GL_FRAMEBUFFER, fbo);
 
+			for (auto i=0; i<BUFFERS; i++)
+			{
+        //assign uniform value with hardcoded indices, use glGetUniformLocation is better but adds more line of codes
+				//uniforms can be automatically removed if not used, thus removes/offsets all the following uniforms !
+        glUniform1i(0,i); //int : (PASSINDEX)
+        glUniform1fv(1, UNIFORM_FLOAT_COUNT, uniforms); // floats "_[UNIFORM_FLOAT_COUNT]"
+        glUniform1i(UNIFORM_FLOAT_COUNT+1+i,i); // samplers b0, b1 ..
+
+				glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureID[i*2+swapped], 0);
+				glRects(-1, -1, 1, 1);
+
+				glActiveTexture(GL_TEXTURE0 + i);
+				glBindTexture(GL_TEXTURE_2D, textureID[i*2+swapped]);
+			}
+
+			swapped = !swapped;
+
+			//blit last buffer (fbo) to the displayed frame buffer (0)
+      //TODO : Blit can cost performances, better render the last buffer directly to the displayed framebuffer (0) ?
+			glBindFramebuffer (GL_DRAW_FRAMEBUFFER, 0);
+			glDrawBuffer      (GL_BACK);
+
+			glBlitFramebuffer (0,0, width,height,
+			                   0,0, width,height,
+			                   GL_COLOR_BUFFER_BIT,
+			                   GL_NEAREST);
+#else
+    glUniform1fv(0, UNIFORM_FLOAT_COUNT, uniforms);
 		glRects(-1, -1, 1, 1);
+#endif
 
 		captureFrame();
 
--- engine/gl.hpp
+++ engine/gl.hpp
@@ -2,6 +2,8 @@
 
 #include <GL/gl.h>
 
+#define BUFFERS 3
+
 // relevant glext.h fragment
 #define APIENTRYP __stdcall *
 typedef char GLchar;
@@ -14,6 +16,21 @@ typedef void(APIENTRYP PFNGLLINKPROGRAMPROC)(GLuint program);
 typedef void(APIENTRYP PFNGLSHADERSOURCEPROC)(GLuint shader, GLsizei count, const GLchar *const *string, const GLint *length);
 typedef void(APIENTRYP PFNGLUSEPROGRAMPROC)(GLuint program);
 typedef void(APIENTRYP PFNGLUNIFORM1FVPROC)(GLint location, GLsizei count, const GLfloat *value);
+#ifdef BUFFERS
+#define GL_FRAMEBUFFER 0x8D40
+#define GL_COLOR_ATTACHMENT0 0x8CE0
+#define GL_READ_FRAMEBUFFER 0x8CA8
+#define GL_DRAW_FRAMEBUFFER 0x8CA9
+typedef void (APIENTRYP PFNGLGENFRAMEBUFFERSEXTPROC) (GLsizei n, GLuint *framebuffers);
+typedef void (APIENTRYP PFNGLBINDFRAMEBUFFEREXTPROC) (GLenum target, GLuint framebuffer);
+typedef void (APIENTRYP PFNGLFRAMEBUFFERTEXTURE2DEXTPROC) (GLenum target, GLenum attachment, GLenum textarget, GLuint texture, GLint level);
+typedef void (APIENTRYP PFNGLBLITFRAMEBUFFERPROC) (GLint srcX0, GLint srcY0, GLint srcX1, GLint srcY1, GLint dstX0, GLint dstY0, GLint dstX1, GLint dstY1, GLbitfield mask, GLenum filter);
+#endif
+#if defined(BUFFERS) || defined(TEXTURE)
+#define GL_TEXTURE0 0x84C0
+typedef void (APIENTRYP PFNGLUNIFORM1IPROC) (GLint location, GLint v0);
+typedef void (APIENTRYP PFNGLACTIVETEXTUREPROC) (GLenum texture);
+#endif
 #ifdef DEBUG
 typedef void (APIENTRYP PFNGLGETSHADERINFOLOGPROC) (GLuint shader, GLsizei bufSize, GLsizei *length, GLchar *infoLog);
 typedef void (APIENTRY *GLDEBUGPROC)(GLenum source,GLenum type,GLuint id,GLenum severity,GLsizei length,const GLchar *message,const void *userParam);
@@ -25,7 +42,14 @@ typedef GLint (APIENTRYP PFNGLGETUNIFORMLOCATIONPROC) (GLuint program, const GLc
 #endif
 // end of glext.h fragment
 
+#ifdef BUFFERS
+#define GL_EXT_FUNCTION_COUNT 14
+#elif defined(TEXTURE)
+#define GL_EXT_FUNCTION_COUNT 10
+#else
 #define GL_EXT_FUNCTION_COUNT 8
+#endif
+
 
 static const char *glExtFunctionNames[] = {
 	"glAttachShader",
@@ -36,6 +60,19 @@ static const char *glExtFunctionNames[] = {
 	"glShaderSource",
 	"glUseProgram",
 	"glUniform1fv",
+
+	#if defined(BUFFERS) || defined(TEXTURE)
+	"glUniform1i",
+	"glActiveTexture",
+	#endif
+
+	#ifdef BUFFERS
+	"glGenFramebuffers",
+	"glBindFramebuffer",
+	"glFramebufferTexture2D",
+	"glBlitFramebuffer",
+	#endif
+
 };
 
 static void *glExtFunctions[GL_EXT_FUNCTION_COUNT];
@@ -48,3 +85,15 @@ static void *glExtFunctions[GL_EXT_FUNCTION_COUNT];
 #define glShaderSource ((PFNGLSHADERSOURCEPROC)glExtFunctions[5])
 #define glUseProgram ((PFNGLUSEPROGRAMPROC)glExtFunctions[6])
 #define glUniform1fv ((PFNGLUNIFORM1FVPROC)glExtFunctions[7])
+
+#if defined(BUFFERS) || defined(TEXTURE)
+#define glUniform1i ((PFNGLUNIFORM1IPROC)glExtFunctions[8])
+#define glActiveTexture ((PFNGLACTIVETEXTUREPROC)glExtFunctions[9])
+#endif
+
+#ifdef BUFFERS
+#define glGenFramebuffers ((PFNGLGENFRAMEBUFFERSEXTPROC)glExtFunctions[10])
+#define glBindFramebuffer ((PFNGLBINDFRAMEBUFFEREXTPROC)glExtFunctions[11])
+#define glFramebufferTexture2D ((PFNGLFRAMEBUFFERTEXTURE2DEXTPROC)glExtFunctions[12])
+#define glBlitFramebuffer ((PFNGLBLITFRAMEBUFFERPROC)glExtFunctions[13])
+#endif
